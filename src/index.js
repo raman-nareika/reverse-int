7ab5fccbd3478c05acda17f699ba6e5afe9d008f
Raman N <roma.nareiko@gmail.com>
2020-02-20 00:24:24 +0300

    impl

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+     let reversed = 0;
+     
+     while (n) {
+        reversed = reversed*10;
+        reversed = reversed + (n % 10);
+        n = Math.floor(n/10);
+     }
+
+     return reversed;
 }
